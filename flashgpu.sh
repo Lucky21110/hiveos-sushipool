9c21fdc56ec3806ac5a0b794d8aa88f27f1623f3
Lucky21110 <Arnaud.Piraprez.ca@gmail.com>
2021-02-01 14:27:58 -0500

    Add files via upload

---
+++ flashgpu.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Author : Arnaud Piraprez
+# Copyright (c) AK Hosting Solutions Inc
+# Script follows here:
+cd /home/miner
+sudo su
+echo "Downloading BIOS"
+wget -N https://github.com/xxx/hiveos-sushipool/raw/master/rx470.rom
+echo "Flashing GPU"
+/root/utils/atiflash -pa /home/miner/rx470.rom -pavbpn=113-AD009HS-XGB
+echo "Rebooting"
+reboot
